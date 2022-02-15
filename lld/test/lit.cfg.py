3b1b89cc14e05ad1933fe7b36f63241c1f54e667
Ayke van Laethem <aykevanlaethem@gmail.com>
2022-02-15 22:28:08 +0100

    [Xtensa] Add LLD linker support

--- lld/test/lit.cfg.py
+++ lld/test/lit.cfg.py
@@ -76,7 +76,8 @@ llvm_config.feature_config(
                           'RISCV': 'riscv',
                           'Sparc': 'sparc',
                           'WebAssembly': 'wasm',
-                          'X86': 'x86'})
+                          'X86': 'x86',
+                          'Xtensa': 'xtensa'})
      ])
 
 # Set a fake constant version so that we get consistent output.
