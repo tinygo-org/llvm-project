0b3b4c635458d2a32ff6ec61bd8801523ca6c3ce
Andrei Safronov <safronov@espressif.com>
2023-06-01 00:43:18 +0300

    [Xtensa] Add emit constant pool option.

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -2364,6 +2364,11 @@ void Clang::AddXtensaTargetArgs(const ArgList &Args,
       }
     }
   }
+
+  if (Args.getLastArg(options::OPT_mtext_section_literals) != nullptr) {
+    CmdArgs.push_back("-mllvm");
+    CmdArgs.push_back("-mtext-section-literals");
+  }
 }
 
 void Clang::DumpCompilationDatabase(Compilation &C, StringRef Filename,
