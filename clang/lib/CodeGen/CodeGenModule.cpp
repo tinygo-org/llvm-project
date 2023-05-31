3a52eb4e62c7b33739a35fe73cd58d3469c2773a
Andrei Safronov <safronov@espressif.com>
2023-06-01 00:42:37 +0300

    [Xtensa] Implement Xtensa ABI lowering.

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -316,6 +316,8 @@ createTargetCodeGenInfo(CodeGenModule &CGM) {
     return createLoongArchTargetCodeGenInfo(
         CGM, Target.getPointerWidth(LangAS::Default), ABIFRLen);
   }
+  case llvm::Triple::xtensa:
+    return createXtensaTargetCodeGenInfo(CGM);
   }
 }
 
