zRedShift/llvm-project-espressif

[Xtensa] Correction of the Hardware Loop pass.
Update loop counter via a phi instruction. This
improvement fix case when loop have multiple enters.

llvm/lib/Target/Xtensa/XtensaISelLowering.cpp
@@ -302,8 +302,12 @@ XtensaTargetLowering::XtensaTargetLowering(const TargetMachine &tm,
     setTargetDAGCombine(ISD::FSUB);
   }
 
-  if (Subtarget.hasSingleFloat() || Subtarget.hasLoop()) {
+  if (Subtarget.hasSingleFloat()) {
     setTargetDAGCombine(ISD::BRCOND);
+  }
+
+  if (Subtarget.hasLoop()) {
+    setTargetDAGCombine(ISD::BR_CC);
   }
 
   // Needed so that we don't try to implement f128 constant loads using
@@ -585,7 +589,7 @@ static SDValue SearchLoopIntrinsic(SDValue N, ISD::CondCode &CC, int &Imm,
   }
   case ISD::INTRINSIC_W_CHAIN: {
     unsigned IntOp = cast<ConstantSDNode>(N.getOperand(1))->getZExtValue();
-    if (IntOp != Intrinsic::loop_decrement)
+    if (IntOp != Intrinsic::loop_decrement_reg)
       return SDValue();
     return N;
   }
@@ -593,16 +597,27 @@ static SDValue SearchLoopIntrinsic(SDValue N, ISD::CondCode &CC, int &Imm,
   return SDValue();
 }
 
-static SDValue PerformBRCONDCombine(SDNode *N, SelectionDAG &DAG,
+static SDValue PerformHWLoopCombine(SDNode *N, SelectionDAG &DAG,
                                     TargetLowering::DAGCombinerInfo &DCI,
                                     const XtensaSubtarget &Subtarget) {
   SDValue Chain = N->getOperand(0);
   SDLoc DL(N);
-  SDValue Cond = N->getOperand(1);
-  SDValue Dest = N->getOperand(2);
+  SDValue Cond;
+  SDValue Dest;
   ISD::CondCode CC = ISD::SETEQ;
   int Imm = 1;
   bool Negate = false;
+
+  assert(N->getOpcode() == ISD::BR_CC && "Expected BR_CC!");
+  CC = cast<CondCodeSDNode>(N->getOperand(1))->get();
+  Cond = N->getOperand(2);
+  Dest = N->getOperand(4);
+  if (auto *Const = dyn_cast<ConstantSDNode>(N->getOperand(3))) {
+    if (!Const->isOne() && !Const->isNullValue())
+      return SDValue();
+    Imm = Const->getZExtValue();
+  } else
+    return SDValue();
 
   SDValue Int = SearchLoopIntrinsic(Cond, CC, Imm, Negate);
   if (Int) {
@@ -634,16 +649,39 @@ static SDValue PerformBRCONDCombine(SDNode *N, SelectionDAG &DAG,
     } else if (!IsFalseIfZero(CC, Imm)) {
       llvm_unreachable("unsupported condition");
     }
+    SDLoc dl(Int);
+    SDValue Elements = Int.getOperand(2);
+    SDValue Size = DAG.getTargetConstant(
+        cast<ConstantSDNode>(Int.getOperand(3))->getZExtValue(), dl, MVT::i32);
+    SDValue Args[] = {
+        Int.getOperand(0),
+        Elements,
+        Size,
+    };
+    SDValue LoopDec = DAG.getNode(XtensaISD::LOOPDEC, dl,
+                                     DAG.getVTList(MVT::i32, MVT::Other), Args);
 
     // We now need to make the intrinsic dead (it cannot be instruction
     // selected).
-    DAG.ReplaceAllUsesOfValueWith(Int.getValue(1), Int.getOperand(0));
-    assert(Int.getNode()->hasOneUse() &&
-           "Counter decrement has more than one use");
+    DAG.ReplaceAllUsesWith(Int.getNode(), LoopDec.getNode());
 
-    return DAG.getNode(XtensaISD::LOOPEND, DL, MVT::Other, N->getOperand(0),
-                       Dest);
+    Chain = DAG.getNode(ISD::TokenFactor, dl, MVT::Other,
+                        SDValue(LoopDec.getNode(), 1), Chain);
+
+    SDValue EndArgs[] = {Chain, SDValue(LoopDec.getNode(), 0), Dest};
+    return DAG.getNode(XtensaISD::LOOPBR, dl, MVT::Other, EndArgs);
   }
+  return SDValue();
+}
+
+static SDValue PerformBRCONDCombine(SDNode *N, SelectionDAG &DAG,
+                                    TargetLowering::DAGCombinerInfo &DCI,
+                                    const XtensaSubtarget &Subtarget) {
+  SDValue Chain = N->getOperand(0);
+  SDLoc DL(N);
+  SDValue Cond = N->getOperand(1);
+  SDValue Dest = N->getOperand(2);
+  ISD::CondCode CC = ISD::SETEQ;
 
   if (Cond.getOpcode() != ISD::SETCC)
     return SDValue();
@@ -671,6 +709,8 @@ SDValue XtensaTargetLowering::PerformDAGCombine(SDNode *N,
     return performADDCombine(N, DAG, DCI, Subtarget);
   case ISD::FSUB:
     return performSUBCombine(N, DAG, DCI, Subtarget);
+  case ISD::BR_CC:
+    return PerformHWLoopCombine(N, DAG, DCI, Subtarget);
   case ISD::BRCOND:
     return PerformBRCONDCombine(N, DAG, DCI, Subtarget);
   }
@@ -1843,6 +1883,8 @@ const char *XtensaTargetLowering::getTargetNodeName(unsigned Opcode) const {
     OPCODE(CMPOEQ);
     OPCODE(CMPOLE);
     OPCODE(CMPOLT);
+    OPCODE(LOOPBR);
+    OPCODE(LOOPDEC);
     OPCODE(LOOPEND);
     OPCODE(MADD);
     OPCODE(MSUB);

tinygo-org/llvm-project

llvm/lib/Target/Xtensa/XtensaISelLowering.cpp
@@ -302,8 +302,12 @@ XtensaTargetLowering::XtensaTargetLowering(const TargetMachine &tm,
     setTargetDAGCombine(ISD::FSUB);
   }
 
-  if (Subtarget.hasSingleFloat() || Subtarget.hasLoop()) {
+  if (Subtarget.hasSingleFloat()) {
     setTargetDAGCombine(ISD::BRCOND);
+  }
+
+  if (Subtarget.hasLoop()) {
+    setTargetDAGCombine(ISD::BR_CC);
   }
 
   // Needed so that we don't try to implement f128 constant loads using
@@ -592,7 +596,7 @@ static SDValue SearchLoopIntrinsic(SDValue N, ISD::CondCode &CC, int &Imm,
   }
   case ISD::INTRINSIC_W_CHAIN: {
     unsigned IntOp = cast<ConstantSDNode>(N.getOperand(1))->getZExtValue();
-    if (IntOp != Intrinsic::loop_decrement)
+    if (IntOp != Intrinsic::loop_decrement_reg)
       return SDValue();
     return N;
   }
@@ -600,16 +604,27 @@ static SDValue SearchLoopIntrinsic(SDValue N, ISD::CondCode &CC, int &Imm,
   return SDValue();
 }
 
-static SDValue PerformBRCONDCombine(SDNode *N, SelectionDAG &DAG,
+static SDValue PerformHWLoopCombine(SDNode *N, SelectionDAG &DAG,
                                     TargetLowering::DAGCombinerInfo &DCI,
                                     const XtensaSubtarget &Subtarget) {
   SDValue Chain = N->getOperand(0);
   SDLoc DL(N);
-  SDValue Cond = N->getOperand(1);
-  SDValue Dest = N->getOperand(2);
+  SDValue Cond;
+  SDValue Dest;
   ISD::CondCode CC = ISD::SETEQ;
   int Imm = 1;
   bool Negate = false;
+
+  assert(N->getOpcode() == ISD::BR_CC && "Expected BR_CC!");
+  CC = cast<CondCodeSDNode>(N->getOperand(1))->get();
+  Cond = N->getOperand(2);
+  Dest = N->getOperand(4);
+  if (auto *Const = dyn_cast<ConstantSDNode>(N->getOperand(3))) {
+    if (!Const->isOne() && !Const->isZero())
+      return SDValue();
+    Imm = Const->getZExtValue();
+  } else
+    return SDValue();
 
   SDValue Int = SearchLoopIntrinsic(Cond, CC, Imm, Negate);
   if (Int) {
@@ -641,16 +656,39 @@ static SDValue PerformBRCONDCombine(SDNode *N, SelectionDAG &DAG,
     } else if (!IsFalseIfZero(CC, Imm)) {
       llvm_unreachable("unsupported condition");
     }
+    SDLoc dl(Int);
+    SDValue Elements = Int.getOperand(2);
+    SDValue Size = DAG.getTargetConstant(
+        cast<ConstantSDNode>(Int.getOperand(3))->getZExtValue(), dl, MVT::i32);
+    SDValue Args[] = {
+        Int.getOperand(0),
+        Elements,
+        Size,
+    };
+    SDValue LoopDec = DAG.getNode(XtensaISD::LOOPDEC, dl,
+                                     DAG.getVTList(MVT::i32, MVT::Other), Args);
 
     // We now need to make the intrinsic dead (it cannot be instruction
     // selected).
-    DAG.ReplaceAllUsesOfValueWith(Int.getValue(1), Int.getOperand(0));
-    assert(Int.getNode()->hasOneUse() &&
-           "Counter decrement has more than one use");
+    DAG.ReplaceAllUsesWith(Int.getNode(), LoopDec.getNode());
 
-    return DAG.getNode(XtensaISD::LOOPEND, DL, MVT::Other, N->getOperand(0),
-                       Dest);
+    Chain = DAG.getNode(ISD::TokenFactor, dl, MVT::Other,
+                        SDValue(LoopDec.getNode(), 1), Chain);
+
+    SDValue EndArgs[] = {Chain, SDValue(LoopDec.getNode(), 0), Dest};
+    return DAG.getNode(XtensaISD::LOOPBR, dl, MVT::Other, EndArgs);
   }
+  return SDValue();
+}
+
+static SDValue PerformBRCONDCombine(SDNode *N, SelectionDAG &DAG,
+                                    TargetLowering::DAGCombinerInfo &DCI,
+                                    const XtensaSubtarget &Subtarget) {
+  SDValue Chain = N->getOperand(0);
+  SDLoc DL(N);
+  SDValue Cond = N->getOperand(1);
+  SDValue Dest = N->getOperand(2);
+  ISD::CondCode CC = ISD::SETEQ;
 
   if (Cond.getOpcode() != ISD::SETCC)
     return SDValue();
@@ -678,6 +716,8 @@ SDValue XtensaTargetLowering::PerformDAGCombine(SDNode *N,
     return performADDCombine(N, DAG, DCI, Subtarget);
   case ISD::FSUB:
     return performSUBCombine(N, DAG, DCI, Subtarget);
+  case ISD::BR_CC:
+    return PerformHWLoopCombine(N, DAG, DCI, Subtarget);
   case ISD::BRCOND:
     return PerformBRCONDCombine(N, DAG, DCI, Subtarget);
   }
@@ -1850,6 +1890,8 @@ const char *XtensaTargetLowering::getTargetNodeName(unsigned Opcode) const {
     OPCODE(CMPOEQ);
     OPCODE(CMPOLE);
     OPCODE(CMPOLT);
+    OPCODE(LOOPBR);
+    OPCODE(LOOPDEC);
     OPCODE(LOOPEND);
     OPCODE(MADD);
     OPCODE(MSUB);